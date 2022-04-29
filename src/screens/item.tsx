170eedc018b45d7cc4fdc627b473e44b3386ce08
Eldes <studio@eldes.com>
2022-04-29 17:25:13 -0300

    [+] Adiciona tela Item

---
+++ src/screens/item.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const ItemScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>Nome do item</Text>
+      <Text>Descrição do item</Text>
+    </View>
+  );
+};
+
+export default ItemScreen;
